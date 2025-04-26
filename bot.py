5783ecef29ec0664cfcda25b5e9d2946143ee69d
Lim Yu Cheng <yuchenglim3004@gmail.com>
2025-04-26 23:54:57 +0800

    UI/UX

--- bot.py
+++ bot.py
@@ -153,8 +153,7 @@ async def run_stages(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await context.bot.send_message(
                 chat_id,
                 f"❓ *Question* {game['questions_asked']}/{game['num_questions']}\n\n{question_text}\n\n"
-                f"💬 Hint: {hint}",
-                parse_mode="Markdown"
+                f"💬 Hint: {hint}"
             )
 
         await asyncio.sleep(12)
